3b2c537f7a599c149533485fb68e021f057c9535
MixKage <shishov265@gmail.com>
2021-04-12 23:23:51 +0300

    Добавьте файлы проекта.

---
+++ WorkWithThreads/WorkWithThreads/Program.cs
@@ -0,0 +1,376 @@
+﻿#define ThreadAndProcess
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Threading;
+using Newtonsoft.Json;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using VkNet.Model.Attachments;
+using VkNet.Model.RequestParams;
+using WorkWithThreads.Objects;
+
+namespace WorkWithThreads
+{
+    internal class Program
+    {
+        private static bool _file1;
+        private static bool _file2;
+        private static bool _file3;
+        private static bool _trigger;
+
+        public static void Main(string[] args)
+        {
+            var threadMain = Thread.CurrentThread;
+            threadMain.Name = "T0 (Main)";
+
+            var arrayIdPost = new List<ulong?>();
+            var arrayObjectIdTexts = new List<ObjectIdText>();
+            var arrayObjectIdPhotos = new List<ObjectIdPhoto>();
+            var arrayObjectIdUrIs = new List<ObjectIdUri>();
+#if ThreadAndProcess
+            Console.WriteLine("P1 T0 - Start");
+            Console.WriteLine("P1 T0 - download posts");
+#endif
+            var api = Authorize();
+            Debug.WriteLine("T0 - download posts");
+            GetInfo(api, arrayIdPost, arrayObjectIdTexts, arrayObjectIdPhotos, arrayObjectIdUrIs);
+
+            ThreadsControl(arrayObjectIdPhotos, arrayObjectIdTexts, arrayObjectIdUrIs);
+#if DEBUG
+            Process.Start("C:\\Users\\user\\Desktop\\OS\\WorkWithThreadsProc2\\WorkWithThreadsProc2\\bin\\Debug\\netcoreapp3.1\\WorkWithThreadsProc2.exe");
+#endif
+
+#if !DEBUG
+            Process.Start("C:\\Users\\user\\Desktop\\OS\\WorkWithThreadsProc2\\WorkWithThreadsProc2\\bin\\Release\\netcoreapp3.1\\WorkWithThreadsProc2.exe");
+#endif
+            while (true)
+            {
+                Thread.Sleep(10000);
+#if ThreadAndProcess
+                Console.WriteLine("P1 T0 - download posts");
+#endif
+                Debug.WriteLine("T0 - download posts");
+                GetInfo(api, arrayIdPost, arrayObjectIdTexts, arrayObjectIdPhotos, arrayObjectIdUrIs);
+            }
+        }
+
+        private static void WaitingThread(object arrInput)
+        {
+            while (true)
+            {
+                if (_trigger) continue;
+                Thread.Sleep(301);
+                if (Thread.CurrentThread.Name == "T1 (Text)")
+                    SetData1InTextFile(arrInput);
+                else if (Thread.CurrentThread.Name == "T2 (Photo)")
+                    SetData2InTextFile(arrInput);
+                else if (Thread.CurrentThread.Name == "T3 (Uri)")
+                    SetData3InTextFile(arrInput);
+            }
+        }
+
+        private static void ThreadsControl(List<ObjectIdPhoto> arrayObjectIdPhotos,
+            List<ObjectIdText> arrayObjectIdTexts, List<ObjectIdUri> arrayObjectIdUrIs)
+        {
+            Thread threadT1 = new Thread(SetData1InTextFile)
+            {
+                Name = "T1 (Text)"
+            };
+            var threadT2 = new Thread(SetData2InTextFile)
+            {
+                Name = "T2 (Photo)"
+            };
+            var threadT3 = new Thread(SetData3InTextFile)
+            {
+                Name = "T3 (Uri)"
+            };
+            var threadT4 = new Thread(ReadDataStaticFile)
+            {
+                Name = "T4 (Read)"
+            };
+            threadT1.Start(arrayObjectIdTexts);
+            threadT2.Start(arrayObjectIdPhotos);
+            threadT3.Start(arrayObjectIdUrIs);
+            threadT4.Start();
+#if ThreadAndProcess
+            Console.WriteLine("P1 T1 - Start");
+            Console.WriteLine("P1 T2 - Start");
+            Console.WriteLine("P1 T3 - Start");
+            Console.WriteLine("P1 T4 - Start");
+#endif
+        }
+
+        private static void ReadDataStaticFile()
+        {
+            while (true)
+            {
+                using (MemoryMappedFile mmf = MemoryMappedFile.CreateOrOpen("Trigger.txt", sizeof(bool)))
+                {
+                    using var acces = mmf.CreateViewAccessor(0, sizeof(bool));
+
+                    _trigger = acces.ReadBoolean(0);
+
+                    if (!_trigger)
+                    {
+                        ReadDataFirstFile();
+                        ReadDataSecondFile();
+                        ReadDataEndFile();
+                        _file1 = false;
+                        _file2 = false;
+                        _file3 = false;
+                        Thread.Sleep(100);
+                        while (!acces.CanWrite)
+                        {
+
+                        }
+                        acces.Write(0, true);
+                    }
+                }
+            }
+
+        }
+
+        private static void ReadDataFirstFile()
+        {
+            while (!_file1)
+            {
+
+            }
+#if ThreadAndProcess
+            Console.WriteLine("P1 T4 - reading T1 File");
+#endif
+            Debug.WriteLine("T4 - reading T1 File");
+            string tempData;
+           
+            using (var streamReader = new StreamReader("text1.json"))
+            {
+                tempData = streamReader.ReadToEnd();
+            }
+
+            if (tempData.Contains("TextPost"))
+            {
+                var restoredPosts = JsonConvert.DeserializeObject<List<ObjectIdText>>(tempData);
+#if !ThreadAndProcess
+                foreach (var restoredPost in restoredPosts)
+                {
+                    Console.WriteLine("ID Поста: " + restoredPost.IdPost + " Текст поста:" +
+                                      restoredPost.TextPost);
+                }
+#endif
+#if ThreadAndProcess
+                Console.WriteLine("P1 T4 - END reading T1 File");
+#endif
+                Debug.WriteLine("T4 - END Read T1 File");
+            }
+            _file1 = false;
+        }
+
+        private static void ReadDataSecondFile()
+        {
+
+            while (!_file2)
+            {
+
+            }
+#if ThreadAndProcess
+            Console.WriteLine("P1 T4 - reading T2 File");
+#endif
+            Debug.WriteLine("T4 - reading T2 File");
+            string tempData;
+            using (var streamReader = new StreamReader("text2.json"))
+            {
+                tempData = streamReader.ReadToEnd();
+            }
+            if (tempData.Contains("PhotoPost"))
+            {
+                var restoredPosts = JsonConvert.DeserializeObject<List<ObjectIdPhoto>>(tempData);
+#if !ThreadAndProcess
+                foreach (var restoredPost in restoredPosts)
+                {
+                    Console.WriteLine("ID Поста: " + restoredPost.IdPost + " Фото поста:" +
+                                      restoredPost.PhotoPost);
+                }
+#endif
+#if ThreadAndProcess
+                Console.WriteLine("P1 T4 - END Read T2 Fil");
+#endif
+                Debug.WriteLine("T4 - END Read T2 File");
+            }
+            _file2 = false;
+        }
+
+        private static void ReadDataEndFile()
+        {
+            while (!_file3)
+            {
+
+            }
+#if ThreadAndProcess
+            Console.WriteLine("P1 T4 - reading T3 File");
+#endif
+            string tempData;
+            using (var streamReader = new StreamReader("text3.json"))
+            {
+                tempData = streamReader.ReadToEnd();
+            }
+            if (tempData.Contains("UriPost"))
+            {
+                var restoredPosts = JsonConvert.DeserializeObject<List<ObjectIdUri>>(tempData);
+#if !ThreadAndProcess
+                foreach (var restoredPost in restoredPosts)
+                {
+                    Console.WriteLine("ID Поста: " + restoredPost.IdPost + " Ссылка поста:" +
+                                      restoredPost.UriPost);
+                }
+#endif
+#if ThreadAndProcess
+                Console.WriteLine("P1 T4 - END reading T3 File");
+#endif
+                Debug.WriteLine("T4 - END Read T3 File");
+            }
+            _file3 = false;
+        }
+
+        private static void SetData1InTextFile(object arrInput)
+        {
+            while (_file1)
+            {
+
+            }
+
+            var arr = (List<ObjectIdText>)arrInput;
+            var jsonString = JsonConvert.SerializeObject(arr);
+#if ThreadAndProcess
+            Console.WriteLine("P1 T1 - writing T1 File");
+#endif
+            Debug.WriteLine("T1 - writing T1 File");
+            using (var streamWriter = new StreamWriter("text1.json"))
+            {
+                streamWriter.WriteLine(jsonString);
+            }
+            _file1 = true;
+#if ThreadAndProcess
+            Console.WriteLine("P1 T1 - END writing T1 File");
+#endif
+            Debug.WriteLine("T1 - END writing T1 File");
+            WaitingThread(arrInput);
+        }
+
+        private static void SetData2InTextFile(object arrInput)
+        {
+            while (_file2)
+            {
+
+            }
+            var arr = (List<ObjectIdPhoto>)arrInput;
+            var jsonString = JsonConvert.SerializeObject(arr);
+#if ThreadAndProcess
+            Console.WriteLine("P1 T2 - writing T2 File");
+#endif
+            Debug.WriteLine("T2 - writing T2 File");
+            using (var streamWriter = new StreamWriter("text2.json"))
+            {
+                streamWriter.WriteLine(jsonString);
+            }
+            _file2 = true;
+#if ThreadAndProcess
+            Console.WriteLine("P1 T2 - END writing T2 File");
+#endif
+            Debug.WriteLine("T2 - END writing T2 File");
+            WaitingThread(arrInput);
+        }
+
+        private static void SetData3InTextFile(object arrInput)
+        {
+            while (_file3)
+            {
+
+            }
+            var arr = (List<ObjectIdUri>)arrInput;
+            var jsonString = JsonConvert.SerializeObject(arr);
+#if ThreadAndProcess
+            Console.WriteLine("P1 T3 - writing T3 File");
+#endif
+            Debug.WriteLine("T3 - writing T3 File");
+            using (var streamWriter = new StreamWriter("text3.json"))
+            {
+                streamWriter.WriteLine(jsonString);
+            }
+            _file3 = true;
+#if ThreadAndProcess
+            Console.WriteLine("P1 T3 - END writing T3 File");
+#endif
+            Debug.WriteLine("T3 - END writing T3 File");
+            WaitingThread(arrInput);
+        }
+
+        private static VkApi Authorize()
+        {
+            var api = new VkApi();
+
+            api.Authorize(new ApiAuthParams
+            {
+                AccessToken = "Token"
+            });
+            return api;
+        }
+
+        private static void GetInfo(VkApi api, List<ulong?> arrayIdPost, List<ObjectIdText> arrayObjectIdTexts, List<ObjectIdPhoto> arrayObjectIdPhotos, List<ObjectIdUri> arrayObjectIdUrIs)
+        {
+            NewsFeed res = default;
+            try
+            {
+                res = api.NewsFeed.Get(new NewsFeedGetParams
+                { Filters = NewsTypes.Post, Count = 100 });
+            }
+            catch
+            {
+                Console.WriteLine("wi-fi disconnected!");
+                Debug.WriteLine("wi-fi disconnected!");
+                Environment.Exit(0);
+            }
+
+            foreach (var item in res.Items)
+            {
+                var isContentId = false;
+                foreach (var item2 in res.Items)
+                {
+                    if (arrayIdPost.Contains(item2.PostId))
+                    {
+                        isContentId = true;
+                    }
+                }
+                if (isContentId)
+                    continue;
+                if (item.PostId != null)
+                {
+                    if (item.Text != "")
+                    {
+                        arrayObjectIdTexts.Add(new ObjectIdText { IdPost = item.PostId, TextPost = item.Text });
+                    }
+
+                    if (item.Attachments != null)
+                        foreach (var attachment in item?.Attachments)
+                        {
+                            if (attachment.Type == typeof(Photo))
+                                arrayObjectIdPhotos.Add(new ObjectIdPhoto
+                                {
+                                    IdPost = item.PostId,
+                                    PhotoPost = ((Photo)attachment.Instance).Sizes.Last().Url.ToString()
+                                });
+
+                            if (attachment.Type == typeof(Link))
+                                arrayObjectIdUrIs.Add(new ObjectIdUri
+                                { IdPost = item.PostId, UriPost = ((Link)attachment.Instance).Uri.ToString() });
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ WorkWithThreadsProc2/WorkWithThreadsProc2/Program.cs
@@ -0,0 +1,214 @@
+﻿#define ThreadAndProcess
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+using WorkWithThreads.Objects;
+
+namespace WorkWithThreadsProc2
+{
+    class Program
+    {
+        private static int _endReadFiles;
+        private static bool _resume;
+        private static bool _start;
+#if DEBUG
+        private static string Path1 = @"C:\Users\user\Desktop\OS\WorkWithThreads\WorkWithThreads\bin\Debug\netcoreapp3.1\text1.json";
+        private static string Path2 = @"C:\Users\user\Desktop\OS\WorkWithThreads\WorkWithThreads\bin\Debug\netcoreapp3.1\text2.json";
+        private static string Path3 = @"C:\Users\user\Desktop\OS\WorkWithThreads\WorkWithThreads\bin\Debug\netcoreapp3.1\text3.json";
+#endif
+#if !DEBUG
+        private static string Path1 = @"C:\Users\user\Desktop\OS\WorkWithThreads\WorkWithThreads\bin\Release\netcoreapp3.1\text1.json";
+        private static string Path2 = @"C:\Users\user\Desktop\OS\WorkWithThreads\WorkWithThreads\bin\Release\netcoreapp3.1\text2.json";
+        private static string Path3 = @"C:\Users\user\Desktop\OS\WorkWithThreads\WorkWithThreads\bin\Release\netcoreapp3.1\text3.json";
+#endif
+        static void ControllerThread()
+        {
+            Thread T1 = new Thread(WaitingThread)
+            {
+                Name = "T1"
+            };
+            Thread T2 = new Thread(WaitingThread)
+            {
+                Name = "T2"
+            };
+            Thread T3 = new Thread(WaitingThread)
+            {
+                Name = "T3"
+            };
+            T1.Start();
+            T2.Start();
+            T3.Start();
+#if ThreadAndProcess
+            Console.WriteLine("P2 T1 - Created");
+            Console.WriteLine("P2 T2 - Created");
+            Console.WriteLine("P2 T3 - Created");
+#endif
+        }
+
+        static void Queue()
+        {
+            while (true)
+            {
+                if (_endReadFiles == 3)
+                {
+                    _resume = true;
+                    _start = false;
+                    return;
+                }
+            }
+        }
+
+        static void WaitingThread()
+        {
+            while (true)
+            {
+                if (_start)
+                {
+                    _endReadFiles = 0;
+                    if (Thread.CurrentThread.Name == "T1")
+                    {
+#if ThreadAndProcess
+                        Console.WriteLine("P2 T1 - Read File 1");
+#endif
+                        Debug.WriteLine("T1");
+                        ReadFirstFile();
+                        _endReadFiles++;
+                        Queue();
+                    }
+                    else if (Thread.CurrentThread.Name == "T2")
+                    {
+#if ThreadAndProcess
+                        Console.WriteLine("P2 T2 - Read File 2");
+#endif
+                        Debug.WriteLine("T2");
+                        ReadSecondFile();
+                        _endReadFiles++;
+                        Queue();
+                    }
+                    else if (Thread.CurrentThread.Name == "T3")
+                    {
+#if ThreadAndProcess
+                        Console.WriteLine("P2 T3 - Read File 3");
+#endif
+                        Debug.WriteLine("T3");
+                        ReadEndFile();
+                        _endReadFiles++;
+                        Queue();
+                    }
+                }
+            }
+        }
+
+
+
+        static void ReadFirstFile()
+        {
+            string tempData;
+
+            using (var streamReader = new StreamReader(Path1))
+            {
+                tempData = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+
+            var restoredPosts = JsonConvert.DeserializeObject<List<ObjectIdText>>(tempData);
+#if !ThreadAndProcess
+                foreach (var restoredPost in restoredPosts)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " ID Поста: " + restoredPost.IdPost +
+                                      " Текст поста: " +
+                                      restoredPost.TextPost);
+                }
+#endif
+        }
+
+        static void ReadSecondFile()
+        {
+            string tempData;
+
+            using (var streamReader = new StreamReader(Path2))
+            {
+                tempData = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+
+            var restoredPosts = JsonConvert.DeserializeObject<List<ObjectIdPhoto>>(tempData);
+#if !ThreadAndProcess
+                foreach (var restoredPost in restoredPosts)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " ID Поста: " + restoredPost.IdPost +
+                                      " Фото поста: " +
+                                      restoredPost.PhotoPost);
+                }
+#endif
+        }
+
+        static void ReadEndFile()
+        {
+            string tempData;
+
+            using (var streamReader = new StreamReader(Path3))
+            {
+                tempData = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+
+            var restoredPosts = JsonConvert.DeserializeObject<List<ObjectIdUri>>(tempData);
+
+#if !ThreadAndProcess
+                foreach (var restoredPost in restoredPosts)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " ID Поста: " + restoredPost.IdPost +
+                                      " Фото поста: " +
+                                      restoredPost.UriPost);
+                }
+#endif
+        }
+
+        static void StartWork()
+        {
+            while (true)
+            {
+                using (MemoryMappedFile mmf = MemoryMappedFile.CreateOrOpen("Trigger.txt", sizeof(bool)))
+                {
+                    using var trigger = mmf.CreateViewAccessor(0, sizeof(bool));
+                    trigger.Read(0, out _start);
+
+                    if (_start)
+                    {
+#if ThreadAndProcess
+                        Console.WriteLine("P2 T0 - Check access");
+#endif
+                        Debug.WriteLine("T0");
+                        Thread.Sleep(50);
+                        _start = false;
+                    }
+
+                    if (_resume)
+                    {
+                        trigger.Write(0, _start);
+                        _resume = false;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ControllerThread();
+#if ThreadAndProcess
+            Console.WriteLine("P2 T0 - Start");
+#endif
+            while (true)
+            {
+                StartWork();
+            }
+        }
+    }
+}
