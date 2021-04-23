289ff72e1600568764f0bf5854a220b815904ce5
MixKage <shishov265@gmail.com>
2021-04-23 17:55:54 +0300

    Финальная сборка. Сервис при выключении завершает работу, реализована транзакционная запись, реализована общая память

---
+++ WorkWithThreads/Deamon/Installer1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ServiceProcess;
+using System.Configuration.Install;
+
+
+namespace Deamon
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : Installer
+    {
+        ServiceInstaller serviceInstaller;
+        ServiceProcessInstaller processInstaller;
+
+        public Installer1()
+        {
+            InitializeComponent();
+            serviceInstaller = new ServiceInstaller();
+            processInstaller = new ServiceProcessInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.StartType = ServiceStartMode.Manual;
+            serviceInstaller.ServiceName = "Daemon";
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
---
+++ WorkWithThreads/ProccesOne/Objects/ObjectIDPhoto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkWithThreads.Objects
+{
+    class ObjectIdPhoto
+    {
+        public ulong? IdPost { get; set; }
+        public string PhotoPost { get; set; }
+      
+    }
+}
